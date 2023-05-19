59fc81b16fa5a5a98627668cd3b096589a7447fe
hwvs <hwvs@localhost>
2023-05-18 18:01:23 -0700

    make valid

--- Invoke-GPTObfuscation.ps1
+++ Invoke-GPTObfuscation.ps1
@@ -1,40 +1,175 @@
-# This file is an interface to use Invoke-GPOObfuscation as a command line tool.
-# It is not intended to be used as a module - instead use Invoke-GPOObfuscation.psm1
+# This file is an interface to use Invoke-GPTObfuscation as a command line tool.
+# It is not intended to be used as a module - instead use Invoke-GPTObfuscation.psm1
 
-# Load the module
-$script_dir = Split-Path -Parent $MyInvocation.MyCommand.Path
-Import-Module "$script_dir\Invoke-GPOObfuscation.psm1"
-
-$prompt_templates = @()
-
-Get-ChildItem -Path $script_dir -Filter "*.prompt_tpl" -File | % {
-    $prompt_templates += $_.FullName
+# Load the module from the file "Invoke-GPTObfuscation.psm1" or PWD
+$script_dir = Split-Path -Parent $MyInvocation.MyCommand.Definition
+$module_path = $script_dir + "/Invoke-GPTObfuscation.psm1"
+if(-not (Test-Path $module_path)) {
+    # Try PWD
+    $module_path = $pwd.Path + "/Invoke-GPTObfuscation.psm1"
+}
+if((Test-Path $module_path)) {
+    Invoke-Expression (Get-Content $module_path -Raw)
+} else {
+    Write-Host "[Error] Invoke-GPTObfuscation.psm1 not found in $script_dir" -ForegroundColor Red
+    return;
 }
 
-if($prompt_templates.Count -eq 0) {
+$prompt_template_paths = @()
+
+Get-ChildItem -Path $script_dir -Filter "*.prompt_tpl" -File | % {
+    $prompt_template_paths += $_.FullName
+}
+
+if($prompt_template_paths.Count -eq 0) {
     Write-Host "[Error] No prompt templates (*.prompt_tpl) found in $script_dir" -ForegroundColor Red
     return;
 }
 
 # Provide a list of prompts to choose from (do while) and then continue on
 write-host "Select a prompt template to use:"
+Write-Host "Q. [Quit / Exit]"
 
 $index = 0
-$prompt_templates | % {
+$prompt_template_paths | % {
     $index++
     write-host "$index. $_"
 }
 
-$prompt_template = $null
+$prompt_template_path = $null
 do {
     $choice = Read-Host "Enter a number between 1 and $index"
+    $choice = $choice.Trim().ToUpper()
+    if($choice -eq 'Q') {
+        write-host "Exiting"
+        return;
+    }
     $choice = [int]$choice
     if($choice -gt 0 -and $choice -le $index) {
-        $prompt_template = $prompt_templates[$choice-1]
+        $prompt_template_path = $prompt_template_paths[$choice-1]
     }
     else {
         write-host "Invalid choice: $choice"
     }
-} while($prompt_template -eq $null)
+} while($prompt_template_path -eq $null)
 
-write-host "Using prompt template: $prompt_template"
+write-host "Using prompt template: $prompt_template_path"
+
+# Get the script to obfuscate
+write-host "How do you want to provide your script?"
+
+# Ask until valid answer, or exit
+$script_input_method = $null
+do {
+    # File (F) or Paste (P)
+    $choice = Read-Host "Pick one of: [F]ile, [P]aste, [Q]uit"
+
+    try {
+        $choice = $choice.Trim().ToUpper().Substring(0,1) # Take the first character, upper case
+    }
+    catch {
+        Write-Host "Invalid choice: $choice" -ForegroundColor Red
+        continue;
+    }
+
+    if($choice -eq 'Q') {
+        write-host "Exiting"
+        return;
+    }
+    elseif($choice -eq 'F' -or $choice -eq 'P') {
+        $script_input_method = $choice # Set the method
+        break;
+    }
+    else {
+        write-host "Invalid choice: $choice"
+    }
+} while($true)
+
+Write-Host "Using input method: " (@("File", "Paste")|? { $_.Substring(0,1) -eq $script_input_method })
+
+switch ($script_input_method) {
+    'F' {
+        # Get the file
+        $script_file = Read-Host "Enter the path to the script file"
+        if(Test-Path $script_file) {
+            $script = Get-Content $script_file | Out-String
+        }
+        else {
+            Write-Host "[Error] Script file not found: $script_file" -ForegroundColor Red
+            return;
+        }
+    }
+    'P' {
+        # Get the script, multiple lines
+        $script = @()
+        do {
+            $line = Read-Host "Enter a line of the script, or a single 'Q' to finish"
+            if($line.Trim().ToUpper() -eq 'Q') {
+                break;
+            }
+            $script += $line
+        } while($true)
+    }
+}
+
+# Sanity Check - make sure the script is valid
+$script = $script.Trim()
+
+# Count the number of non-white space characters
+$script_length = ($script | Measure-Object -Character).Characters
+if($script_length -eq 0) {
+    Write-Host "[Error] Script is empty" -ForegroundColor Red
+    return;
+}
+elseif($script_length -lt 100) {
+    Write-Host "[Warning] Script is less than 100 characters long - Are you sure you want to continue? (Y/n)" -ForegroundColor Yellow
+    $choice = Read-Host
+    if($choice.Trim().ToUpper() -ne 'Y' -and $choice.Trim().ToUpper() -ne 'YES') {
+        Write-Host "Exiting!"
+        return;
+    }
+}
+elseif($script_length -gt 10000) {
+    Write-Host "[Warning] Script is greater than 10,000 characters long - Are you sure you want to continue? (Y/n)" -ForegroundColor Yellow
+    $choice = Read-Host
+    if($choice.Trim().ToUpper() -ne 'Y' -and $choice.Trim().ToUpper() -ne 'YES') {
+        Write-Host "Exiting!"
+        return;
+    }
+}
+
+# Offer to save the script to a file if the input method was paste
+if ($script_input_method -eq "P") {
+    do {
+        $choice = Read-Host "Do you want to save the script to a file? (Y/n)"
+        if($choice.Trim().ToUpper() -eq 'Y' -or $choice.Trim().ToUpper() -eq 'YES') {
+            $script_file = Read-Host "Enter the path to save the script file"
+            if(Test-Path $script_file) {
+                Write-Host "[Error] Script file already exists: $script_file" -ForegroundColor Red
+                continue;
+            }
+            else {
+                $script | Out-File $script_file
+                Write-Host "Script saved to $script_file"
+                break;
+            }
+        }
+        else {
+            break;
+        }
+    } while($true)
+}
+
+# Ask for the provider (disabled)
+#$provider = Read-Host "Enter the provider to use ('OpenAI', )
+
+
+# TODO: Instead, store a list of things to replace and get context for each, so variables are consistent.
+
+# Okay, let's do this
+$script_obfuscated = Invoke-GPTObfuscation -ScriptBlock $script -PromptTemplateFile $prompt_template_path -Verbose $true
+
+# Output the obfuscated script
+Write-Host "Obfuscated script:"
+Write-Host $script_obfuscated
+
--- Invoke-GPTObfuscation.psm1
+++ Invoke-GPTObfuscation.psm1
@@ -1,8 +1,8 @@
 <#
     .Synopsis
-        Obfuscate PowerShell code using the Invoke-GPOObfuscation function.
+        Obfuscate PowerShell code using the Invoke-GPTObfuscation function.
     .Description
-        This function will obfuscate PowerShell code using the Invoke-GPOObfuscation function.
+        This function will obfuscate PowerShell code using the Invoke-GPTObfuscation function.
     .Parameter ScriptCode
         The script block to obfuscate.
     .Parameter PromptTemplateFile
@@ -21,20 +21,271 @@
         Optional. The AI provider to use. Defaults to "OpenAI".
     .Parameter AIProviderKey
         Optional. The API key for the AI provider. Defaults to the environment variable OPENAI_API_KEY.
+    .Parameter Verbose
+        Optional. If true, verbose output will be displayed. Defaults to true.
 
     .Example
-        Invoke-GPOObfuscation -ScriptBlock {$str = "Hello World!"; Write-Host $str}
+        Invoke-GPTObfuscation -ScriptBlock {$str = "Hello World!"; Write-Host $str}
 #>
+function Invoke-GPTObfuscation {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory=$true, ValueFromPipeline=$true)]
+        [object]
+        $ScriptBlock,
+
+        [Parameter(Mandatory=$false)]
+        [string]
+        $PromptTemplateFile = '',
+
+        [Parameter(Mandatory=$false)]
+        [string]
+        $PromptTemplate = '',
+
+        [Parameter(Mandatory=$false)]
+        [hashtable]
+        $PromptSettings,
+        [Parameter(Mandatory=$false)]
+        [bool]
+        $ShouldSplit = $false,
+        [Parameter(Mandatory=$false)]
+        [int]
+        $SplitSize = 1000,
+        [Parameter(Mandatory=$false)]
+        [hashtable]
+        $AIProviderSettings,
+        [Parameter(Mandatory=$false)]
+        [string]
+        $AIProvider = 'OpenAI',
+        [Parameter(Mandatory=$false)]
+        [string]
+        $AIProviderKey = ''
+    )
+
+    # Default to verbose output
+    if($Verbose -eq $null) {
+        $Verbose = $true
+    }
+
+    if($PromptTemplate -eq $true) {
+        $PromptTemplate = '' #weird
+    }
+    if($PromptTemplateFile -eq $true) {
+        $PromptTemplateFile = ''#weird
+    }
+
+    # Cannot pass a file and template string at the same time
+    if(($PromptTemplateFile -ne $null -and $PromptTemplateFile.Length -gt 0) -and ($PromptTemplate -ne $null -and $PromptTemplate.Length -gt 0)) {
+        throw "Cannot pass a file and template string at the same time (ambiguous): $PromptTemplateFile, $PromptTemplate"
+        return;
+    }
+    elseif($PromptTemplateFile -eq $null -or $PromptTemplateFile.Length -eq 0) {
+        if($PromptTemplate -eq $null -or $PromptTemplate.Length -eq 0) {
+            throw "Must pass a file or template string"
+            return;
+        }
+    }
+
+    $AIProviderObj = $null
+    if($AIProvider -eq $null -or $AIProvider -eq '' -or $AIProvider -eq 'OpenAI') { # Default to OpenAI
+        $AIProviderObj = [OpenAIProvider]::new()
+    }
+    else {
+        # Not implemented yet
+        throw "AI provider not implemented yet: $AIProvider"
+        return;
+    }
+
+    # Initialize AI provider
+    #Set setting for each
+    if($AIProviderSettings -ne $null) {
+        foreach($setting in $AIProviderSettings.Keys) {
+            $AIProviderObj.$setting = $AIProviderSettings[$setting]
+        }
+    }
+
+    # Set the API key if param is set
+    if($AIProviderKey -ne $null -and $AIProviderKey -ne "") {
+        $AIProviderObj.SetSetting("APIKey", $AIProviderKey)
+    }
+
+    
+    if($ScriptBlock -isnot [string] -and $ScriptBlock -isnot [System.Management.Automation.ScriptBlock] -and $ScriptBlock -isnot [array]) {
+        throw "ScriptBlock must be either a string or System.Management.Automation.ScriptBlock"
+        return;
+    }
+
+    if ($ScriptBlock -is [System.Management.Automation.ScriptBlock]) {
+        $script = $ScriptBlock.ToString()
+        $script = $script.Trim()
+    }
+    else {
+        $script = $ScriptBlock
+    }
+    
+    $PromptTemplateContent = $null
+
+    # Validate the prompt template file
+    if($PromptTemplateFile -ne $null -and $PromptTemplateFile -ne "") {
+        write-host "PromptTemplateFile: $PromptTemplateFile" -ForegroundColor Cyan
+        if((Test-Path $PromptTemplateFile)) {
+            $PromptTemplateContent = (Get-Content $PromptTemplateFile) | Out-String
+        }
+        else {
+            throw "Prompt template file not found: $PromptTemplateFile"
+            return;
+        }
+    }
+    else {
+        $PromptTemplateContent = $PromptTemplate
+    }
+
+    # Validate the prompt template
+    if($PromptTemplateContent -eq $null -or $PromptTemplateContent -eq "") {
+        if($Verbose) {
+            Write-Error "[Error] No prompt template provided"
+        }
+        else {
+            throw "No prompt template provided"
+        }
+        return;
+    }
+
+    # Validate the prompt settings
+    if($PromptSettings -eq $null) {
+        $PromptSettings = @{}
+    }
+
+    # Validate the AI provider
+    if($AIProvider -eq "OpenAI") {
+        if($AIProviderKey -eq $null -or $AIProviderKey -eq "") {
+            # Check for environment variable
+            $AIProviderKey = $env:OPENAI_API_KEY
+            if($AIProviderKey -ne $null -and $AIProviderKey.Length -gt 0) {
+                if($Verbose) {
+                    Write-Host "Using API key from environment variable OPENAI_API_KEY" -ForegroundColor Cyan
+                }
+            }
+            else {
+                if($Verbose) {
+                    Write-Error "[Error] No API key provided for OpenAI (set the OPENAI_API_KEY environment variable or pass the parameter AIProviderKey)"
+                }
+                else {
+                    throw "No API key provided for OpenAI"
+                }
+                return;
+            }
+        }
+    }
+
+
+
+    # Split the script into blocks
+    if(-not $ShouldSplit) {
+        $SplitSize = 99999999; # large number
+    }
+    $script_blocks =  Split-ScriptIntoBlocks -script $script -max_block_length $SplitSize -Verbose:$Verbose
+
+
+    # Write what we have 
+    if($Verbose) {
+        $total_length = $script_blocks | Measure-Object -Sum Length | Select-Object -ExpandProperty Sum
+        Write-Host "Blocks: $($script_blocks.Count), Total length: $total_length"
+    }
+
+    $result = @()
+    #  generate the completion
+    foreach($block in $script_blocks) {
+        
+        # Generate the prompt
+        $prompt = $PromptTemplateContent
+        foreach($setting in $PromptSettings.Keys) {
+            $prompt = $prompt.Replace("{{" + $setting + "}}", $PromptSettings[$setting])
+        }
+        # Replace {{SCRIPT_INPUT}}
+        $prompt = $prompt.Replace("{{SCRIPT_INPUT}}", (($block -Join "") | Out-String))
+
+        $completion = $AIProviderObj.GenerateCompletion($prompt)
+        $result += @($completion)
+    }
+
+    return ($result -Join "`n").Trim()
+
+
+}
+
+
+function Split-ScriptIntoBlocks($script, [int]$max_block_length = 2000, [bool]$Verbose = $true) {
+    
+    # Split the script into lines and remove carriage returns (if any)
+    if($script -isnot [array]) { # If the script is a single object, split it into lines
+        $script = $script -split "`n"
+    }
+
+    # Split into blocks of (up-to) 2000 characters, line-by-line #TODO: Make this configurable. Make this take {} into account. Take block comments into account.
+    $script_blocks = @()
+
+    $current_block = ""
+    $script | % {
+        # print a warning if the line is too long
+        if($_.Length -gt $max_block_length) {
+            Write-Host "[Warning] Line is longer than $max_block_length characters: $_" -ForegroundColor Yellow
+        }
+
+        # If the block is empty, add the line
+        if($current_block.Length -eq 0) {
+            $current_block = $_
+            return; # Skip to the next line
+        }
+
+        # If the block is too long, add the block to the list and start a new block
+        if($current_block.Length + $_.Length -gt $max_block_length) {
+            $script_blocks += $current_block
+            $current_block = $_
+        }
+        # Otherwise, add the line to the block
+        else {
+            $current_block += $_
+        } 
+    }
+
+    # Add the last block to the list
+    if($current_block.Length -gt 0) {
+        $script_blocks += $current_block
+    }
+
+    # Remove empty blocks
+    $script_blocks = $script_blocks | ? { $_.Length -gt 0 }
+
+
+    # Print the number of blocks
+    if($script_blocks.Count -eq 1) {
+        
+    }
+    elseif ($script_blocks.Count -eq 0) {
+        if($Verbose) {
+            Write-Error "[Error] No script blocks found (This shouldn't happen??)" 
+        }
+        else {
+            throw "No script blocks found (This shouldn't happen??)"
+        }
+        return;
+    }
+    else {
+        Write-Host "[Warning] Script split into $($script_blocks.Count) blocks" -ForegroundColor Yellow
+        Write-Host "This is not properly supported yet, so the script will most likely be broken due to missing context." -ForegroundColor Yellow
+    }
+
+    return $script_blocks
+            
+}
+
+
+
 
 # Virtual class to override
 class AIProvider {
     [hashtable] $Settings = @{}
-
-    AIProvider($new_settings) {
-        foreach($Settings in $new_settings.Keys) {
-            $this.SetSetting($Settings, $new_settings[$Settings])
-        }
-    }
 
     [void] SetSetting([string] $name, [object] $value) {
         $this.Settings[$name] = $value
@@ -52,109 +303,69 @@ class AIProvider {
         }
     }
 
-    [string] GenerateCompletion([string] $prompt, [string] $script) {
+    [string] GenerateCompletion([string] $prompt) {
         throw "NotImplemented"
     }
 }
 
 # OpenAI class
-class OpenAIProvider {
+class OpenAIProvider : AIProvider {
+
+
     # GenerateCompletion impl
-    [string] GenerateCompletion([string] $prompt, [string] $script) {
+    [string] GenerateCompletion([string] $prompt) {
+
+        if($prompt -eq 'True') {
+            throw "Error, `$prompt is 'True', this is not right."
+            return $null;
+        }
+
         $API_KEY = $this.GetSetting("APIKey", $env:OPENAI_API_KEY)
         if($API_KEY -eq $null -or $API_KEY -eq "") {
             throw "No API key provided for OpenAI"
         }
-        $headers = @{"Authorization" = "Bearer $API_KEY"}
-        $body = @{
-            "prompt" = $prompt
-            "max_tokens" = $this.GetSetting("MaxTokens", 1024)
-            "temperature" = $this.GetSetting("Temperature", 0.1)
-            "top_p" = 1.0
-            "n" = 1
-            "stream" = $false
-            "logprobs" = $null
-            "stop" = $this.GetSetting("StopSeqs", @('---','```'))
+        $headers = @{
+            "Authorization" = "Bearer $API_KEY"
+            "Content-Type" = "application/json"
         }
-        $body = $body | ConvertTo-Json
-        $response = Invoke-RestMethod -Method Post -Uri "https://api.openai.com/v1/engines/davinci/completions" -Headers $headers -Body $body
-        return $response.choices[0].text
-    }
 
-}
 
-function Invoke-GPOObfuscation {
-    [CmdletBinding()]
-    param(
-        [Parameter(Mandatory=$true, ValueFromPipeline=$true)]
-        [System.Management.Automation.ScriptBlock]
-        $ScriptBlock,
-        [Parameter(Mandatory=$false)]
-        [string]
-        $PromptTemplateFile,
-        [Parameter(Mandatory=$false)]
-        [string]
-        $PromptTemplate,
-        [Parameter(Mandatory=$false)]
-        [hashtable]
-        $PromptSettings,
-        [Parameter(Mandatory=$false)]
-        [bool]
-        $ShouldSplit = $false,
-        [Parameter(Mandatory=$false)]
-        [int]
-        $SplitSize = 1000,
-        [Parameter(Mandatory=$false)]
-        [hashtable]
-        $AIProviderSettings,
-        [Parameter(Mandatory=$false)]
-        [string]
-        $AIProvider = $null,
-        [Parameter(Mandatory=$false)]
-        [string]
-        $AIProviderKey = $env:OPENAI_API_KEY
-    )
+        $model = $this.GetSetting("Model", 'text-davinci-003')
+        $max_tokens = $this.GetSetting("MaxTokens", 1500)
+        $temperature = $this.GetSetting("Temperature", 0.1)
+        $stop = $this.GetSetting("StopSeqs", @('---','```'))
 
-    if($AIProvider -eq $null) {
-        $AIProvider = New-Object 'AIProvider' $AIProviderSettings
         
-    }
+        $request_body = @{
+            "model" = $model
+            "prompt" = $prompt
+            "temperature" = $temperature
+            "max_tokens" = $max_tokens
+            "stop" = $stop
 
-    # Validate the script block
-    $script = $ScriptBlock.ToString()
-    $script = $script.Trim()
-    $script = $script.TrimStart("{")
-    $script = $script.TrimEnd("}")
-    $script = $script.Trim()
+            "stream"=$false
+            "top_p"=1
+            "n"=1
+        } | ConvertTo-Json
 
-    # Validate the prompt template file
-    if($PromptTemplateFile -ne $null -and $PromptTemplateFile -ne "") {
-        if(Test-Path $PromptTemplateFile) {
-            $PromptTemplate = Get-Content $PromptTemplateFile | Out-String
+        # Get the response
+        $endpoint_uri = 'https://api.openai.com/v1/completions'
+        
+        write-verbose "Sending request to $endpoint_uri"
+        write-verbose "Request body: $request_body"
+        write-verbose "Headers: $headers"
+
+        $response = Invoke-RestMethod -Uri $endpoint_uri -Method Post -Headers $headers -Body $request_body
+        
+        # Check for error
+        if($response.choices -eq $null -or $response.choices.Count -eq 0) {
+            throw "No response from OpenAI"
         }
-        else {
-            Write-Host "[Error] Prompt template file not found: $PromptTemplateFile" -ForegroundColor Red
-            return;
-        }
-    }
 
-    # Validate the prompt template
-    if($PromptTemplate -eq $null -or $PromptTemplate -eq "") {
-        Write-Host "[Error] No prompt template provided" -ForegroundColor Red
-        return;
-    }
+        write-verbose "Response: $($response | convertto-json)"
 
-    # Validate the prompt settings
-    if($PromptSettings -eq $null) {
-        $PromptSettings = @{}
-    }
+        return (($response.choices | % { $_.text } ) -Join "")
 
-    # Validate the AI provider
-    if($AIProvider -eq "OpenAI") {
-        if($AIProviderKey -eq $null -or $AIProviderKey -eq "") {
-            Write-Host "[Error] No API key provided for OpenAI" -ForegroundColor Red
-            return;
-        }
     }
 
 }
